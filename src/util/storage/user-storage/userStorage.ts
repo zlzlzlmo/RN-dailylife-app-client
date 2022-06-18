273792ccba4f4e152a5bc5f3e55d6f270d2118c4
신승훈 <zlzlzlmo@daum.net>
2022-06-19 08:38:16 +0900

    jwt 담을 user storage class 생성

---
+++ src/util/storage/user-storage/userStorage.ts
@@ -0,0 +1,31 @@
+import Storage from "../../storage/Storage";
+
+export interface IToken {
+  accessToken: string;
+  refreshToken: string;
+}
+
+class UserStorage extends Storage<IToken> {
+  setAccessToken(value: string): void {
+    this.set("accessToken", value);
+  }
+
+  getAccessToken(): string | null | undefined {
+    return this.get("accessToken");
+  }
+
+  setRefreshToken(value: string): void {
+    this.set("refreshToken", value);
+  }
+
+  getRefreshToken(): string | null | undefined {
+    return this.get("refreshToken");
+  }
+
+  setAllToken({ accessToken, refreshToken }: IToken): void {
+    this.setAccessToken(accessToken);
+    this.setRefreshToken(refreshToken);
+  }
+}
+
+export default UserStorage;
