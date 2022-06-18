0a769720c4d499c88fd0b40f017489c7f2ee8572
신승훈 <zlzlzlmo@daum.net>
2022-06-19 08:34:28 +0900

    Storage 추상 클래스 생성

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["babel-plugin-styled-components"],
+    plugins: [
+      ["babel-plugin-styled-components"],
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { API_URL } from "@env";
 
-export const instance = axios.create({
-  baseURL: "",
+const axiosSetting = {
+  baseURL: API_URL,
   headers: {
     "Content-type": "application/json",
   },
-});
+};
+export const instance = axios.create(axiosSetting);
