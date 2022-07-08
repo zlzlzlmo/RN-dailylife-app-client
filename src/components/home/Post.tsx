294f6806d226309fbe79e60879ef82da05c67386
신승훈 <zlzlzlmo@daum.net>
2022-07-09 08:37:01 +0900

    feat: render dummy images list

--- src/components/home/Post.tsx
+++ src/components/home/Post.tsx
@@ -58,6 +58,28 @@ const PostContent = () => {
         natus? Ullam consectetur modi maxime ducimus, incidunt quia numquam ut
         amet eligendi nihil vitae? Eius facere possimus placeat illo fuga.
       </Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://media.istockphoto.com/photos/businesss-desk-office-with-notebook-and-cactustop-view-table-from-picture-id829374196?k=20&m=829374196&s=170667a&w=0&h=eG3x-IbGc6WaLShW52hUTyIFa36mqBOnEVaEotrZNR4=",
+          }}
+        />
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://media.istockphoto.com/photos/businesss-desk-office-with-notebook-and-cactustop-view-table-from-picture-id829374196?k=20&m=829374196&s=170667a&w=0&h=eG3x-IbGc6WaLShW52hUTyIFa36mqBOnEVaEotrZNR4=",
+          }}
+        />
+
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://media.istockphoto.com/photos/businesss-desk-office-with-notebook-and-cactustop-view-table-from-picture-id829374196?k=20&m=829374196&s=170667a&w=0&h=eG3x-IbGc6WaLShW52hUTyIFa36mqBOnEVaEotrZNR4=",
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -132,6 +154,17 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     marginVertical: 15,
+  },
+  imageContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
+  image: {
+    width: "32%",
+    minHeight: 100,
+    maxHeight: 200,
+    resizeMode: "contain",
   },
   tagsContainer: {
     flexDirection: "row",
---
+++ src/mocks/post-dummy.ts
@@ -0,0 +1,3 @@
+interface IPost {
+  profileImage: string;
+}
