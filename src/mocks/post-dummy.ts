7b10a9b33519e4dfd27d420b808bf4f19fb7e70e
신승훈 <zlzlzlmo@daum.net>
2022-07-09 08:46:13 +0900

    feat: post item interface

--- src/mocks/post-dummy.ts
+++ src/mocks/post-dummy.ts
@@ -1,3 +1,47 @@
-interface IPost {
+interface IPostImage {
+  id: string;
+  url: string;
+}
+
+interface ITag {
+  id: string;
+  name: string;
+}
+
+interface IUser {
+  userId: string;
+  userName: string;
   profileImage: string;
 }
+
+interface IComment extends IUser {
+  content: string;
+}
+
+interface IPost extends IUser {
+  id: string;
+  createdAt: string;
+  images: IPostImage[];
+  tags: ITag[];
+  like: number;
+  comments: IComment[];
+}
+
+export const DUMMY_POST: IPost[] = [
+  {
+    userId: "Seunghoon",
+    profileImage: "",
+    userName: "Seunghoon",
+    id: "1",
+    createdAt: "2021-02-04",
+    images: [
+      {
+        id: "1",
+        url: "https://media.istockphoto.com/photos/businesss-desk-office-with-notebook-and-cactustop-view-table-from-picture-id829374196?k=20&m=829374196&s=170667a&w=0&h=eG3x-IbGc6WaLShW52hUTyIFa36mqBOnEVaEotrZNR4=",
+      },
+    ],
+    tags: [{ id: "1", name: "font-end" }],
+    like: 9,
+    comments: [],
+  },
+];
